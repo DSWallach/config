0d5c99f899109f347618efcfdd64cd02ee87a6d6
DSWallach <d.s.t.wallach@gmail.com>
2018-11-30 10:12:12 -0500

    Updated setup for prezto

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
-ln -sf $HOME/.sel/vim/vim $HOME/.vim
-ln -sf $HOME/.sel/vim/vimrc $HOME/.vimrc
-ln -sf $HOME/.sel/zsh/zshrc $HOME/.zshrc
-ln -sf $HOME/.sel/tmux/tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/.sel/git/gitconfig $HOME/.gitconfig
+$name=.sel
+
+git clone ssh://github.com/dswallach/config $HOME/$name
+
+ln -sf $HOME/$name/vim/vim $HOME/.vim
+ln -sf $HOME/$name/vim/vimrc $HOME/.vimrc
+ln -sf $HOME/$name/zsh/zshrc $HOME/.zshrc
+ln -sf $HOME/$name/tmux/tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/$name/git/gitconfig $HOME/.gitconfig
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+etopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
