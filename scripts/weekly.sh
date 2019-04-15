465cbed1aa1be93e246841ce09b50cd1e7894f37
dswallach <d.s.t.wallach@gmail.com>
2019-04-15 13:36:54 -0400

    Update

--- scripts/weekly.sh
+++ scripts/weekly.sh
@@ -5,8 +5,18 @@ cd ~/.sel;
 git pull -y;
 
 ##  Tasks
+
 # Blog
 task add project:blog.post tag:weekly due:Sunday Write one blog post
+task add project:blog.comment tag:weekly due:Sunday "Comment on someone else's blog post"
+task add project:blog.comment tag:weekly due:Sunday "Comment on someone else's blog post"
+task add project:blog.comment tag:weekly due:Sunday "Comment on someone else's blog post"
+
+# Real Estate
+task add project:income.rental tag:weekly due:Sunday "Find a deal in the Hudson Valley"
+task add project:income.rental tag:weekly due:Sunday "Find a deal in the Hudson Valley"
+task add project:income.rental tag:weekly due:Sunday "Find a deal in the Hudson Valley"
+
 
 # Programming
 task add project:BeA3 tag:Programming due:Sunday "Complete one medium algorithms type problem from some coding site (leetcode, codility, etc)"
