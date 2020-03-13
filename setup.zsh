afb19ca768e91e767ecd6f118d1fa1a857e79455
dswallach <d.s.t.wallach@gmail.com>
2020-03-13 14:04:37 -0400

    I really need to fix this setup script

--- setup.zsh
+++ setup.zsh
@@ -11,20 +11,20 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc" | sudo tee /etc/yum.repos.d/mongodb.repo
 
 # Install necessary packages
-if [ $0 = "full" ]; then
+#if [ $0 = "full" ]; then
 	#if [ $(hostname) != "node2-3@minerva.hpc.mssm.edu" ]; then
-		sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y;
-		sudo dnf upgrade -y;
-		sudo dnf install cmake gcc-c++ make python3-devel zsh vim neovim tmux util-linux-user \
-        task fzf mongodb-org mariadb mariadb-server gstreamer1-libav gstreamer1-plugins-bad-free \
-           gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras \
-                  gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-nonfree \
-                         gstreamer1-plugins-good gstreamer1-plugins-ugly lame-libs lame-libs -y;
-    sudo dnf group upgrade --with-optional Multimedia -y;
-	#else
+  sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y;
+  sudo dnf upgrade -y;
+  sudo dnf install cmake gcc-c++ make python3-devel zsh vim gvim tmux util-linux-user \ 
+      task fzf mongodb-org mariadb mariadb-server gstreamer1-libav gstreamer1-plugins-bad-free \ 
+      gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free-extras \ 
+      gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-nonfree \ 
+      gstreamer1-plugins-good gstreamer1-plugins-ugly lame-libs lame-libs -y;
+        sudo dnf group upgrade --with-optional Multimedia -y;
+        #else
 	#	module use /hpc/packages/minerva-common/modulefiles;
 	#fi
-fi
+#fi
 
 # Clone my config repo
 if [ ! -d ${HOME}/${name} ]; then
@@ -47,7 +47,7 @@ ln -sf $HOME/$name/zsh/zpreztorc $HOME/.zpreztorc
 ln -sf $HOME/$name/ssh/config $HOME/.ssh/config
 ln -sf $HOME/$name/ctags $HOME/.ctags
 
-if [ $0 = "full" ]; then
+#if [ $0 = "full" ]; then
 	# Setup for vim and neovim
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -61,4 +61,4 @@ if [ $0 = "full" ]; then
 	chsh -s /bin/zsh
 
   zsh "${HOME}/${name}/zsh/antigen.zsh"
-fi
+#fi
